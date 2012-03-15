3bce7d29ee32e3279ea9d527c1232dbbf914e78b
Beta Yumatsud <hcksx908@yahoo.co.jp>
2012-03-15 11:48:44 +0900

    Artifacts of study in the morning

---
+++ Android/primary/GoodbyeAndroid/src/example/android/goodbye/GoodbyeAndroid.java
@@ -0,0 +1,13 @@
+package example.android.goodbye;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GoodbyeAndroid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.goodbye);
+    }
+}
