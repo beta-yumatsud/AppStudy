1d9ed6e727a70c60316420d9488f06e4a1ef8a77
Beta Yumatsud <hcksx908@yahoo.co.jp>
2012-03-15 12:12:36 +0900

    Artifacts of study in the morning

---
+++ Android/primary/LayoutViewTest/src/example/android/layoutviewtest/LayoutViewTest.java
@@ -0,0 +1,13 @@
+package example.android.layoutviewtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LayoutViewTest extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layoutviewtest);
+    }
+}
