6100abbdb4837ab2e0389d4d03d9cb636433edff
Beta Yumatsud <hcksx908@yahoo.co.jp>
2012-03-15 11:49:58 +0900

    Artifacts of study in the morning

---
+++ Android/primary/GoodMorning/src/example/android/goodmorning/GoodMorningActivity.java
@@ -0,0 +1,13 @@
+package example.android.goodmorning;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GoodMorningActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.goodmorninglayout);
+    }
+}
