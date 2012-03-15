db840ffd79facbd464edb4b794a7fb1c5d1864a2
Beta Yumatsud <hcksx908@yahoo.co.jp>
2012-03-15 12:18:29 +0900

    Artifacts of study in the morning

---
+++ Android/primary/TableLayoutSample/src/example/andorid/tablelayoutsample/TableLayoutSample.java
@@ -0,0 +1,13 @@
+package example.andorid.tablelayoutsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TableLayoutSample extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tablelayoutsample);
+    }
+}
