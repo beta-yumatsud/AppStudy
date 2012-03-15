04a17104895c37a73f1878c5e62162ad873f0a30
Beta Yumatsud <hcksx908@yahoo.co.jp>
2012-03-15 12:17:48 +0900

    Artifacts of study in the morning

---
+++ Android/primary/TabelViewTest/src/example/android/tablelayoutviewsample/TabelViewTest.java
@@ -0,0 +1,13 @@
+package example.android.tablelayoutviewsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TabelViewTest extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tableviewtest);
+    }
+}
