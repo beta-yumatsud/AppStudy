dbe55a576b102da0beca7abbd0430b1741066d4d
Beta Yumatsud <hcksx908@yahoo.co.jp>
2012-03-15 12:14:22 +0900

    Artifacts of study in the morning

---
+++ Android/primary/MynameAndroid/src/example/android/myname/MynameAndroidActivity.java
@@ -0,0 +1,13 @@
+package example.android.myname;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MynameAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myname);
+    }
+}
