feda4e5444e2f8bc0e339e7f6585949c54e8b020
Beta Yumatsud <hcksx908@yahoo.co.jp>
2012-03-15 12:16:05 +0900

    Artifacts of study in the morning

---
+++ Android/primary/OptionMenuSample/src/example/android/optionmenusample/OptionMenuSampleActivity.java
@@ -0,0 +1,96 @@
+package example.android.optionmenusample;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+
+public class OptionMenuSampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.optionmenusample);
+    }
+    
+    // オプションメニュー生成
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+    	super.onCreateOptionsMenu(menu);
+    	
+    	// メニューアイテム1の追加
+    	MenuItem item1 = menu.add(0, 0, 0, "item1");
+    	item1.setIcon(android.R.drawable.ic_menu_crop);
+    	
+    	// メニューアイテム2の追加
+    	MenuItem item2 = menu.add(0, 1, 0, "item2");
+    	item2.setIcon(android.R.drawable.ic_menu_search);
+    	
+    	// メニューアイテム3の追加
+    	MenuItem item3 = menu.add(0, 2, 0, "item3");
+    	item3.setIcon(android.R.drawable.ic_menu_save);
+    	
+    	// メニューアイテム4の追加
+    	MenuItem item4 = menu.add(0, 3, 0, "item4");
+    	item4.setIcon(android.R.drawable.ic_menu_call);
+    	
+    	// メニューアイテム5の追加
+    	MenuItem item5 = menu.add(0, 4, 0, "item5");
+    	item5.setIcon(android.R.drawable.ic_menu_camera);
+    	
+    	// メニューアイテム6の追加
+    	SubMenu item6 = menu.addSubMenu(0, 5, 0, "その他");
+    	item6.setIcon(android.R.drawable.ic_menu_more);
+    	item6.add(0, 10, 0, "subitem1");
+    	item6.add(0, 20, 0, "subitem2");
+    	
+    	return true;
+    }
+    
+    // メニューアイテム選択処理
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	switch( item.getItemId() ){
+    		case 0:
+    			showDialog("メニューアイテム1を選択しました。");
+    			return true;
+    		case 1:
+    			showDialog("メニューアイテム2を選択しました。");
+    			return true;
+    		case 2:
+    			showDialog("メニューアイテム3を選択しました。");
+    			return true;
+    		case 3:
+    			showDialog("メニューアイテム4を選択しました。");
+    			return true;
+    		case 4:
+    			showDialog("メニューアイテム5を選択しました。");
+    			return true;
+    		case 10:
+    			showDialog("サブメニューアイテム1を選択しました。");
+    			return true;
+    		case 20:
+    			showDialog("サブメニューアイテム2を選択しました。");
+    			return true;
+    	}
+    	return true;
+    }
+    
+    // ダイアログ表示
+    private void showDialog(String text){
+    	AlertDialog.Builder dialog = new AlertDialog.Builder(OptionMenuSampleActivity.this);
+    	dialog.setTitle("メニューアイテム選択結果");
+    	dialog.setMessage(text);
+    	dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface arg0, int arg1) {
+				OptionMenuSampleActivity.this.setResult(Activity.RESULT_OK);
+			}
+		});
+    	dialog.create();
+    	dialog.show();
+    }
+}
