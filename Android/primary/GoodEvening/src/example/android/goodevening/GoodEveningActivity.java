d6a1285abcbee50ed3800d18701ba7947d2cbbfd
Beta Yumatsud <hcksx908@yahoo.co.jp>
2012-03-15 11:49:21 +0900

    Artifacts of study in the morning

---
+++ Android/primary/GoodEvening/src/example/android/goodevening/GoodEveningActivity.java
@@ -0,0 +1,13 @@
+package example.android.goodevening;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GoodEveningActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.goodeveninglayout);
+    }
+}
