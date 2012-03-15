b205f468cda87197d8b9ecd7ded0b8f19c3851e8
Beta Yumatsud <hcksx908@yahoo.co.jp>
2012-03-15 11:40:24 +0900

    Artifacts of study in the morning

---
+++ Android/primary/ContentProviderSample1/src/example/android/contentprovidersample1/ProductContentProvider.java
@@ -0,0 +1,60 @@
+package example.android.contentprovidersample1;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+
+public class ProductContentProvider extends ContentProvider{
+	// CreateProductHelperオブジェクト
+	CreateProductHelper databaseHelper;
+	
+	// データベースの作成処理
+	@Override
+	public boolean onCreate(){
+		databaseHelper = new CreateProductHelper(getContext());
+		return true;
+	}
+	
+	// データベースへの登録処理
+	@Override
+	public Uri insert(Uri arg0, ContentValues values){
+		SQLiteDatabase db = databaseHelper.getWritableDatabase();
+		db.insert("product", null, values);
+		return null;
+	}
+	
+	// データベースからデータ取得処理
+	@Override
+	public Cursor query(Uri arg0, String[] projection, String selection, String[] selectionArgs, String sortOrder){
+		SQLiteDatabase db = databaseHelper.getReadableDatabase();
+		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+		qb.setTables("product");
+		Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, null);
+		return c;
+	}
+	
+	// データベースの更新処理：今回は未使用
+	@Override
+	public int update(Uri arg0, ContentValues arg1, String arg2, String[] arg3){
+		SQLiteDatabase db = databaseHelper.getWritableDatabase();
+		int row = db.update("product", arg1, arg2, arg3);
+		return row;
+	}
+	
+	// データベースの削除処理：今回は未使用
+	@Override
+	public int delete(Uri arg0, String arg1, String[] arg2){
+		SQLiteDatabase db = databaseHelper.getWritableDatabase();
+		int row = db.delete("product", arg1, arg2);
+		return row;
+	}
+	
+	// 種別の取得処理：今回は未使用
+	@Override
+	public String getType(Uri arg0){
+		return null;
+	}
+}
